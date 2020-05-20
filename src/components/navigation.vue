3d2c560aafb41d7c8257e85bb80b9dff2449fe20
wyk <martywyk@gmail.com>
2020-05-20 08:45:30 -0300

    minor changes

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -3,7 +3,7 @@
  * @Author: wyk
  * @Date: 2020-05-15 14:40:18
  * @LastEditors: wyk
- * @LastEditTime: 2020-05-18 09:43:36
+ * @LastEditTime: 2020-05-19 14:55:35
 -->
 <template>
   <div>
@@ -26,7 +26,7 @@ export default {
 
 <style scoped lang="scss">
 div {
-  height: 0.5rem;
+  height: 0.3rem;
   background-color: black;
   border-radius: 15px;
   width: 75%;
